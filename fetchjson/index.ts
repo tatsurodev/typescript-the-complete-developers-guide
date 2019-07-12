e7db60fbccaf6e790e099a4f5341ac5f5e8e5962
Tatsuro <contacts@tatsuro.dev>
2019-07-12 23:05:07 +0900

    無名関数の引数に型宣言を追加してerrorをcatch 8

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -16,9 +16,13 @@ axios.get(url).then(response => {
   const title = todo.title;
   const completed = todo.completed;
 
+  logTodo(id, completed, title);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
         The Todo with ID: ${id}
         Has a title of: ${title}
         Is it finished? ${completed}
     `);
-});
+};
